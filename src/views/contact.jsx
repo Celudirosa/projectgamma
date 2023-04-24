cefba6725cb12d76fef8e9b6d01ce774f6445616
Celudirosa <cluqued@live.com>
2023-04-24 14:46:46 +0200

    no va el contact

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -1,8 +1,3 @@
 export default function Contact() {
-  return (
-    <h1>
-      Hola, soy el contact
-      <p>contactameeeeeeee que molo</p>
-    </h1>
-  );
+  return <h1>Hola, soy el contact</h1>
 }
